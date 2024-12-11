ebf37606b50eb237c63dd47203fcc45b40f1eebd
Eduardofbarreto <seuemail@example.com>
2024-12-11 12:00:37 -0300

    ex logica 5

--- new.cpp
+++ new.cpp
@@ -2,22 +2,26 @@
 
 int main(){
 
-using namespace std;
+    float num1, num2, num3, soma, subt, mult, div;
 
-    float num1, num2, div;
+    std::cout<<"Digite um valor para num1: ";
+    std::cin>>num1;
 
-    cout<<"Digite um valor para num1: ";
-    cin>>num1;
+    std::cout<<"Digite um valor para num2: ";
+    std::cin>>num2;
 
-    cout<<"Digite um valor para num2: ";
-    cin>>num2;
+    std::cout<<"Digite um valor para num3: ";
+    std::cin>>num3;
 
-    
+    //Bloco de resoluções
+    soma = num1 + num2 + num3;
+    subt = num1 - num2 - num3;
+    mult = num1 * num2 * num3;
+    div = num1 / num2 / num3;
 
-   
-    div = num1 / num2;
-
-   
-    cout<<"\O resultado da divisao e: "<<div<<endl;
+    std::cout<<"\nO resultado da soma e: "<<soma<<std::endl;
+    std::cout<<"O resultado da subtracao e: "<<subt<<std::endl;
+    std::cout<<"O resultado da multiplicacao e: "<<mult<<std::endl;
+    std::cout<<"O resultado da divisao e: "<<div<<std::endl;
 
 }
