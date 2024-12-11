500abaa02fd7685e535e929d6aaba3aa58597f51
Eduardofbarreto <seuemail@example.com>
2024-12-11 11:43:19 -0300

    ex logica 3

--- new.cpp
+++ new.cpp
@@ -4,7 +4,7 @@ int main(){
 
 using namespace std;
 
-    int num1, num2, num3, soma, subt, mult;
+    float num1, num2, num3, soma, subt, mult, div;
 
     cout<<"Digite um valor para num1: ";
     cin>>num1;
@@ -12,15 +12,16 @@ using namespace std;
     cout<<"Digite um valor para num2: ";
     cin>>num2;
 
-    cout<<"Digite um valor para num3: ";
-    cin>>num3;
+    
 
-    soma = num1 + num2 + num3;
-    subt = num1 - num2 + num3;
-    mult = num1 * num2 + num3;
+    soma = num1 + num2;
+    subt = num1 - num2;
+    mult = num1 * num2;
+    div = num1 / num2;
 
     cout<<"\nO resultado da soma e: "<<soma<<endl;
     cout<<"O resultado da subtracao e: "<<subt<<endl;
     cout<<"O resultado da multiplicacao e: "<<mult<<endl;
+    cout<<"O resultado da divisao e: "<<div<<endl;
 
 }
