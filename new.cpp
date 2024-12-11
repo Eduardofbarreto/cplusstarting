ce46bb2c9935752d2a854bc0865581e018377ba5
Eduardofbarreto <seuemail@example.com>
2024-12-11 11:25:41 -0300

    ex logica

---
+++ new.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+
+using namespace std;
+
+    int num1, num2, soma;
+
+    cout<<"Digite um valor para num1: ";
+    cin>>num1;
+
+    cout<<"Digite um valor para num2: ";
+    cin>>num2;
+
+    soma = num1 + num2;
+
+    cout<<"O resultado da soma e: "<<soma<<endl;
+
+}
