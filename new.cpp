17f70edfde607414d124f3a6547bcd36992108c4
Eduardofbarreto <seuemail@example.com>
2024-12-11 11:45:26 -0300

    ex logica 4

--- new.cpp
+++ new.cpp
@@ -4,7 +4,7 @@ int main(){
 
 using namespace std;
 
-    float num1, num2, num3, soma, subt, mult, div;
+    float num1, num2, div;
 
     cout<<"Digite um valor para num1: ";
     cin>>num1;
@@ -14,14 +14,10 @@ using namespace std;
 
     
 
-    soma = num1 + num2;
-    subt = num1 - num2;
-    mult = num1 * num2;
+   
     div = num1 / num2;
 
-    cout<<"\nO resultado da soma e: "<<soma<<endl;
-    cout<<"O resultado da subtracao e: "<<subt<<endl;
-    cout<<"O resultado da multiplicacao e: "<<mult<<endl;
-    cout<<"O resultado da divisao e: "<<div<<endl;
+   
+    cout<<"\O resultado da divisao e: "<<div<<endl;
 
 }
