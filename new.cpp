232f74231e1f11451503d641348e57ed6e670325
Eduardo Filippsen Barreto <fbarreto.eduardo@gmail.com>
2024-12-12 09:12:05 -0300

    media de abastecimento 2

--- new.cpp
+++ new.cpp
@@ -5,15 +5,16 @@ int main() {
     
     float litros, km, media;
 
-    std::cout<<"Digite a quantidade de litros abastecida: ";
-    std::cin>>litros;
-
     std::cout<<"Digite a quilometragem do veículo: ";
     std::cin>>km;
 
+    std::cout<<"Digite a quantidade de litros abastecida: ";
+    std::cin>>litros;
+
+
     media = (km/litros);
 
-    std::cout<<"A media do veículo e de: "<<media<<std::endl;
+    std::cout<<"A media do veículo e de: "<<media<<" km/l."<<std::endl;
 
     return 0;
 }
