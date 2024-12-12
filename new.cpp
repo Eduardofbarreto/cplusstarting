547ec7bc33df0f81cad3cd35de534c60ae1a5a9e
Eduardo Filippsen Barreto <fbarreto.eduardo@gmail.com>
2024-12-12 09:02:37 -0300

    media de abastecimento

--- new.cpp
+++ new.cpp
@@ -3,26 +3,17 @@
 
 int main() {
     
-    int a, a_q; b, b_q, c, c_q, soma;
+    float litros, km, media;
 
-    std::cout<<"Digite um valor para a: ";
-    std::cin>>a;
+    std::cout<<"Digite a quantidade de litros abastecida: ";
+    std::cin>>litros;
 
-    std::cout<<"Digite um valor para b: ";
-    std::cin>>b;
+    std::cout<<"Digite a quilometragem do veículo: ";
+    std::cin>>km;
 
-    std::cout<<"Digite um valor para c: ";
-    std::cin>>c;
+    media = (km/litros);
 
-    a_q = pow(a, 2);
-    b_q = pow(b, 2);
-    c_q = pow(c, 2);
-
-    soma = a_q + b_q + c_q;
-
-    std::cout<<"\nO resultado e: "<<soma<<std::endl;
-    
-
+    std::cout<<"A media do veículo e de: "<<media<<std::endl;
 
     return 0;
 }
